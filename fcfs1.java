18b7b2130942cccfd7bde5da324ca839a978cbc8
abhay8463 <abhay8463@gmail.com>
2020-01-30 20:06:18 +0530

    second commit

---
+++ fcfs1.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+class FCFS1
+{	
+	public static void main(String args[])
+	{	
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number of processes : ");
+		int n = sc.nextInt(),burst[] = new int[n],arrival[] = new int[n],i,no[] = new int[n],w[] = new int[n],t[] = new int[n],temp,gnatt[] = new int[n+1];
+		
+		System.out.println("Enter the Burst Time for");
+		for(i=0;i<n;i++)
+		{  System.out.print("Process "+(i+1)+" : ");   burst[i]=sc.nextInt();  no[i]=i;      }
+		System.out.println("\nEnter the Arrival Time for");
+		for(i=0;i<n;i++)
+		{	System.out.print("Process "+(i+1)+" : ");		arrival[i]=sc.nextInt();		}
+		for(i=0;i<n;i++)
+			for(int j=0;j<n-i-1;j++)
+				if(arrival[j]>arrival[j+1])
+				{
+					temp=arrival[j];
+					arrival[j]=arrival[j+1];
+					arrival[j+1]=temp;
+					temp=no[j];
+					no[j]=no[j+1];
+					no[j+1]=temp;
+					temp=burst[j];
+					burst[j]=burst[j+1];
+					burst[j+1]=temp;
+				}
+		temp=0;
+		gnatt[0]=arrival[0];
+		for(i=1;i<=n;i++)
+		{	for(int j=0;j<i;j++)
+			{
+				temp+=burst[j];
+				if(j==0)
+				{
+					temp+=arrival[0];
+				}
+			}
+			gnatt[i]=temp;	    temp=0;	 
+		}
+		w[0]=0;
+		for(i=0;i<n;i++)
+			w[i]=gnatt[i]-arrival[i];
+		for(i=0;i<n;i++)
+			t[i]=gnatt[i+1]-arrival[i];
+		System.out.print("\nGnatt Chart :\nProcess - \t    ");
+		if(arrival[0]!=0)
+		{
+			System.out.print("\b\b\b\b\b\bidle Time\t  ");
+		}
+		for(i=0;i<n;i++)
+			System.out.print("P"+(no[i]+1)+"\t    ");
+		System.out.print("\nTime - \t\t");
+		{
+			System.out.print("\b\b0\t\t");
+		}
+		if(arrival[0]!=0)
+		{
+			for(i=0;i<=n;i++)
+			System.out.print(gnatt[i]+"\t");
+		}
+		else
+		{
+			for(i=1;i<=n;i++)
+			System.out.print(gnatt[i]+"\t");
+		}
+		
+		
+		System.out.println("\n\nProcess\tBurst Time\tArrival Time\tTurnaround Time\t\tWaiting Time");
+		for(i=0;i<n;i++)
+			System.out.println((no[i]+1)+"\t"+burst[i]+"\t\t"+arrival[i]+"\t\t"+t[i]+"\t\t\t"+w[i]);
+		float at=0,aw=0;
+		for(i=0;i<n;i++)
+		{
+			at+=t[i];		 aw+=w[i];	
+		}
+		System.out.println("\nAverage Turnaround Time = "+(at/=n)+"\nAverage Waiting Time = "+(aw/=n));      
+	}      
+}
